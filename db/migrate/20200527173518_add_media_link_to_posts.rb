7c2ea93cbeecc22eea7a28109b3dc137e85df5fa
lee-injae <ilee.injae@gmail.com>
2020-10-11 18:40:31 -0400

    cleaned up db migration tables

--- db/migrate/20200527173518_add_media_link_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddMediaLinkToPosts < ActiveRecord::Migration[6.0]
-  def change
-    add_column :posts, :media_link, :string
-  end
-end
--- db/migrate/20200528175339_drop_follows_table.rb
+++
@@ -1,5 +0,0 @@
-class DropFollowsTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :follows
-  end
-end
